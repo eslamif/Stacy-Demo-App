e86be7c30d9d350cdbac420d71fd25b64e249be4
Frank Eslami <eslamifrank@gmail.com>
2021-12-12 21:08:21 -0800

    Initial Commit

---
+++ StacyDemo/StacyDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  StacyDemoApp.swift
+//  StacyDemo
+//
+//  Created by Frank Eslami on 12/12/21.
+//
+
+import SwiftUI
+
+@main
+struct StacyDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
