857c7ae4fc383e96e866597cb17937ae4e8d21a2
Frank Eslami <eslamifrank@gmail.com>
2021-12-13 19:25:40 -0800

    Onboarding view works.

---
+++ StacyDemo/OnboardingItem.swift
@@ -0,0 +1,14 @@
+//
+//  OnboardingItem.swift
+//  StacyDemo
+//
+//  Created by Frank Eslami on 12/13/21.
+//
+
+import Foundation
+
+struct OnboardingItem {
+    let imageName: String
+    let title: String
+    let description: String
+}
--- StacyDemo/OnboardingView.swift
+++ StacyDemo/OnboardingView.swift
@@ -8,18 +8,23 @@
 import SwiftUI
 
 struct OnboardingView: View {
+    var onBoardingData: [OnboardingItem] = [
+        OnboardingItem(imageName: "onb_find_place", title: "Find Places to Live", description: "Find great verified places & people to share a home with."),
+        OnboardingItem(imageName: "onb_match", title: "Match Your Preferences", description: "Tell us your preferences and match with the right people."),
+        OnboardingItem(imageName: "onb_like", title: "Like-Minded People", description: "Live together with people who will inspire like you.")
+    ]
+    
     var body: some View {
         VStack {
-            Text("Find Places to Live")
-                .font(.title)
-                .foregroundColor(.primary)
-                .bold()
-                .padding()
-            
-            Text("Find great verified places & people to share a home with.")
-                .font(.body)
-                .foregroundColor(.primary)
-                .padding(.horizontal, 15)
+            TabView {
+                ForEach(0 ..< onBoardingData.count) { index in		
+                    let item = onBoardingData[index]
+                    OnboardingCard(onboardingItem: item)
+                }
+            }
+            //Tab view style
+            .tabViewStyle(PageTabViewStyle(indexDisplayMode: .automatic))
+            .indexViewStyle(PageIndexViewStyle(backgroundDisplayMode: .always))
         }
     }
 }
@@ -27,5 +32,31 @@ struct OnboardingView: View {
 struct OnboardingView_Previews: PreviewProvider {
     static var previews: some View {
         OnboardingView()
+            .previewInterfaceOrientation(.portrait)
+    }
+}
+
+fileprivate struct OnboardingCard: View {
+    let onboardingItem: OnboardingItem
+    
+    var body: some View {
+        VStack {
+            Image(onboardingItem.imageName)
+                .resizable()
+                .frame(height: 400)
+                .frame(maxWidth: .infinity)
+            
+            Text(onboardingItem.title)
+                .font(.title)
+                .foregroundColor(Color("title"))
+                .bold()
+                .padding()
+            
+            Text(onboardingItem.description)
+                .font(.body)
+                .foregroundColor(Color("text"))
+                .multilineTextAlignment(.center)
+                .padding(.horizontal, 15)
+        }
     }
 }
